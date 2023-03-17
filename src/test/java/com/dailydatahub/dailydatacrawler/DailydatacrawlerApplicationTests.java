61e6586fe3f5b88a9d4fd2b175169a38391a8e98
OHSeungHyun <osh1107@hyosung.com>
2023-03-17 15:52:24 +0900

    first commit

---
+++ src/test/java/com/dailydatahub/dailydatacrawler/DailydatacrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailydatahub.dailydatacrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailydatacrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
