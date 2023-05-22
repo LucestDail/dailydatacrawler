c8be32a603e4062458689d102ec3a835bb9aab4f
OHSeungHyun <osh1107@hyosung.com>
2023-05-22 17:43:24 +0900

    schdule annotation migrate from batch to default application context

--- src/main/java/com/dailydatahub/dailydatacrawler/DailydatacrawlerApplication.java
+++ src/main/java/com/dailydatahub/dailydatacrawler/DailydatacrawlerApplication.java
@@ -2,8 +2,10 @@ package com.dailydatahub.dailydatacrawler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DailydatacrawlerApplication {
 
 	public static void main(String[] args) {
